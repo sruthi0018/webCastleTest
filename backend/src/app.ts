a33d1341e13de04d27f2ca86bbb4befd2603e6dc
sruthi0018 <sruthisanil18@gmail.com>
2025-10-24 10:08:04 +0530

    cors err fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,12 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "https://webcastle-front-end.vercel.app/", 
-    credentials: true,
-  })
-);
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 
 connectDB(process.env.MONGO_URI || "");
